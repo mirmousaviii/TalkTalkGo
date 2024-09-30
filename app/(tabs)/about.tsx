f58c3b7b83697f2772c4d4882bed84c1903cb26d
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2024-09-30 20:00:59 +0200

    fix: update content

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,34 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet } from 'react-native';
+
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">About</ThemedText>
+      </ThemedView>
+      <ThemedText>
+          This app is under development. you can contribute to this project by adding new features or fixing bugs by reporting them.
+      </ThemedText>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
