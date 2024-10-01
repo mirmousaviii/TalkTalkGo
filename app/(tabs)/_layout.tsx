ae1ef6930b15d63847611518e77c64ad0dbde32a
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2024-10-01 14:11:27 +0200

    style: change icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -28,7 +28,7 @@ export default function TabLayout() {
                 options={{
                     title: 'Scene',
                     tabBarIcon: ({color, focused}) => (
-                        <TabBarIcon name={focused ? 'menu' : 'menu-outline'} color={color}/>
+                        <TabBarIcon name={focused ? 'book' : 'book-outline'} color={color}/>
                     ),
                 }}
             />
